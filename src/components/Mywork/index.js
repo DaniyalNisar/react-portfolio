07012b7db321d50ac6e80495a453efa84316178c
unknown <2017n5890@gmail.com>
2025-05-02 21:17:52 +0500

    added image to my work page, and also add some minor hover effect

--- src/components/Mywork/index.js
+++ src/components/Mywork/index.js
@@ -15,25 +15,29 @@ const MyWork = () => {
       id: 1,
       title: 'Why I Built This Portfolio',
       date: 'April 28, 2025',
-      excerpt: 'Sharing the story and motivation behind building my own portfolio site using modern web technologies...'
+      excerpt: 'Sharing the story and motivation behind building my own portfolio site using modern web technologies...',
+      image: '/images/blogs/react.png'
     },
     {
       id: 2,
       title: 'Working with React and SCSS',
       date: 'April 25, 2025',
-      excerpt: 'A deep dive into how SCSS enhances styling flexibility and keeps your React project scalable...'
+      excerpt: 'A deep dive into how SCSS enhances styling flexibility and keeps your React project scalable...',
+      image: '/images/blogs/react.png'
     },
     {
       id: 3,
       title: 'Tips for Clean JavaScript Code',
       date: 'April 20, 2025',
-      excerpt: 'These practices help you write cleaner, more maintainable JavaScript in any project...'
+      excerpt: 'These practices help you write cleaner, more maintainable JavaScript in any project...',
+      image: '/images/blogs/react.png'
     },
     {
       id: 4,
       title: 'Designing for Developers',
       date: 'April 18, 2025',
-      excerpt: 'Bridging the gap between design and code can unlock new levels of creativity and productivity...'
+      excerpt: 'Bridging the gap between design and code can unlock new levels of creativity and productivity...',
+      image: '/images/blogs/react.png'
     }
   ];
 
@@ -87,6 +91,7 @@ const MyWork = () => {
                   key={post.id}
                   style={{ transitionDelay: `${index * 0.2}s` }}
                 >
+                  <img src={post.image} alt={post.title} className="work-image" />
                   <h2>{post.title}</h2>
                   <p className="work-date">{post.date}</p>
                   <p className="work-excerpt">{post.excerpt}</p>
